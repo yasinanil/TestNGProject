a7b090a4c57211d6332e2af2953245db37c20d95
Talentify <talentify@gmail.com>
2024-09-23 19:52:23 +0300

    C06_Attributes

---
+++ src/test/java/tests/C06_Attributes.java
@@ -0,0 +1,84 @@
+package tests;
+
+import org.testng.annotations.AfterGroups;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class C06_Attributes {
+
+    @BeforeGroups(groups = "A")
+    public void beforeGroup() {
+        System.out.println("Before Group A");
+    }
+
+    @AfterGroups(groups = "A")
+    public void afterGroup() {
+        System.out.println("After Group A");
+    }
+
+
+    @Test(priority = 4)
+    void test01() {
+        System.out.println("Test 01");
+    }
+
+    @Test(priority = 3, groups = "A")
+    void test02() {
+        System.out.println("Test 02");
+    }
+
+    @Test(priority = 2, groups = "A")
+    void test03() {
+        System.out.println("Test 03");
+    }
+
+    @Test(priority = 1, dependsOnMethods = "test01")
+    void test04() {
+        System.out.println("Test 04");
+    }
+
+    @Test(enabled = false)
+    void test05() {
+        System.out.println("Test 05");
+    }
+
+    @Test(timeOut = 2000)
+    void test06() throws InterruptedException {
+        Thread.sleep(1000);//ThreadTimeoutException
+        System.out.println("Test 06");
+    }
+
+    @Test(invocationCount = 10)
+    void test07() {
+        System.out.println("Test 07");
+    }
+
+    @DataProvider(name = "data")
+    public Object[][] provideData() {
+        return new Object[][]{{"data1", "dataA"}, {"data2", "dataB"}, {"data3", "dataC"}};
+    }
+
+    @Test(dataProvider = "data")
+    void test08(String arg1, String arg2) {
+        System.out.println("Test 08");
+        System.out.println(arg1+"-"+arg2);
+        System.out.println();
+    }
+
+    @Test(expectedExceptions = {ArithmeticException.class})
+    void test09() {
+        System.out.println("Test 09");
+        int x = 1/0;
+    }
+
+    @Test(expectedExceptions = {NullPointerException.class})
+    void test10() {
+        System.out.println("Test 10");
+        String str = null;
+        System.out.println(str.substring(0));
+    }
+
+
+
+}
